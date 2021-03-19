5bd944f510576e6f517508abf7a53d8ad3344e43
Josh Bailey <josh@vandervecken.com>
2021-03-19 19:00:58 +1300

    Implement JSON commands/responses:
    {"command": "sensors"}
    
    {"command":"sensors","error":"","rpiCurrent":1236.48,"supplyVoltage":21.23961}
    
    xxx
    
    {"command":"unknown","error":"InvalidInput"}
    
    {"command": "123"}
    
    {"command":"123","error":"unknown command"}

--- bbsleepypi/bbsleepypi.ino
+++ bbsleepypi/bbsleepypi.ino
@@ -1,27 +1,70 @@
 #include <ArduinoJson.h>
-#include "SleepyPi2.h"
+#include <SleepyPi2.h>
 #include <TimeLib.h>
 #include <LowPower.h>
 #include <PCF8523.h>
 #include <Wire.h>
 
-DynamicJsonDocument sensorDoc(128);
+byte i = 0;
+char cmdBuffer[64] = {};
+
+const char sensorsCmd[] = "sensors";
+
+DynamicJsonDocument inDoc(128);
+DynamicJsonDocument outDoc(128);
 
 void sensorDump() {
-  sensorDoc["response"] = "sensors";
-  sensorDoc["rpiCurrent"] = SleepyPi.rpiCurrent();
-  sensorDoc["supplyVoltage"] = SleepyPi.supplyVoltage();
-  serializeJson(sensorDoc, Serial);
-  Serial.println();
+  outDoc["rpiCurrent"] = SleepyPi.rpiCurrent();
+  outDoc["supplyVoltage"] = SleepyPi.supplyVoltage();
 }
 
 void setup() {
+  memset(cmdBuffer, 0, sizeof(cmdBuffer));
   SleepyPi.enablePiPower(true);
   SleepyPi.rtcInit(false);
   Serial.begin(115200);
 }
 
 void loop() {
-  sensorDump();
-  delay(1000);
+  bool gotCmd = false;
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (i == (sizeof(cmdBuffer) - 1)) {
+      c = 0;
+    }
+    if (c == '\n') {
+      c = 0;
+    }
+    cmdBuffer[i] = c;
+    if (c == 0) {
+      gotCmd = true;
+      i = 0;
+    } else {
+      ++i;
+    }
+  }
+  if (gotCmd) {
+    inDoc.clear();
+    DeserializationError error = deserializeJson(inDoc, cmdBuffer);
+    outDoc.clear();
+    outDoc["command"] = "unknown";
+    if (error) {
+      outDoc["error"] = error.f_str();
+    } else {
+      char *cmd = inDoc["command"];
+      if (cmd) {
+        outDoc["command"] = cmd;
+        outDoc["error"] = "";
+        if (strncmp(cmd, sensorsCmd, strlen(sensorsCmd)) == 0) {
+          sensorDump();
+        } else {
+          outDoc["error"] = "unknown command";
+        }
+      } else {
+        outDoc["error"] = "missing command";
+      }
+    }
+    serializeJson(outDoc, Serial);
+    Serial.println();
+  }
 }
