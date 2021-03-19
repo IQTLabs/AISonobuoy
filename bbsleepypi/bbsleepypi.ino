4f84a5bc3ddffe06d18fec356ed2c4ded644753a
Josh Bailey <josh@vandervecken.com>
2021-03-19 15:28:18 +1300

    Enable Pi power, dump sensors.
    Next version will add JSON command parsing (set RTC clock, etc).

--- bbsleepypi/bbsleepypi.ino
+++ bbsleepypi/bbsleepypi.ino
@@ -1,13 +1,27 @@
-
+#include <ArduinoJson.h>
 #include "SleepyPi2.h"
 #include <TimeLib.h>
 #include <LowPower.h>
 #include <PCF8523.h>
 #include <Wire.h>
 
-void setup() {
+DynamicJsonDocument sensorDoc(128);
+
+void sensorDump() {
+  sensorDoc["response"] = "sensors";
+  sensorDoc["rpiCurrent"] = SleepyPi.rpiCurrent();
+  sensorDoc["supplyVoltage"] = SleepyPi.supplyVoltage();
+  serializeJson(sensorDoc, Serial);
+  Serial.println();
 }
 
+void setup() {
+  SleepyPi.enablePiPower(true);
+  SleepyPi.rtcInit(false);
+  Serial.begin(115200);
+}
 
 void loop() {
+  sensorDump();
+  delay(1000);
 }
