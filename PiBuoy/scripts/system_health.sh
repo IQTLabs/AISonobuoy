a80685c1b660fa69100e1f4c314ec4ed7f52c3ca
cglewis <clewis@iqt.org>
2021-05-05 09:03:32 -0700

    naming and error checking

--- PiBuoy/scripts/system_health.sh
+++ PiBuoy/scripts/system_health.sh
@@ -5,10 +5,10 @@ hostname=$(hostname)
 output_dir=/telemetry/system/"$hostname-$timestamp"
 mkdir -p "$output_dir"
 
-timeout 5 df > "$output_dir"/df.txt
-timeout 5 ifconfig -a > "$output_dir"/ifconfig.txt
-timeout 5 vcgencmd measure_temp > "$output_dir"/temp.txt
-timeout 5 vcgencmd measure_volts > "$output_dir"/volts.txt
-timeout 5 uptime > "$output_dir"/uptime.txt
-timeout 5 free > "$output_dir"/mem.txt
-timeout 10 raspinfo > "$output_dir"/raspinfo.txt
+timeout 5 df > "$output_dir/$hostname-$timestamp"-df.txt
+timeout 5 ifconfig -a > "$output_dir/$hostname-$timestamp"-ifconfig.txt
+timeout 5 vcgencmd measure_temp > "$output_dir/$hostname-$timestamp"-temp.txt
+timeout 5 vcgencmd measure_volts > "$output_dir/$hostname-$timestamp"-volts.txt
+timeout 5 uptime > "$output_dir/$hostname-$timestamp"-uptime.txt
+timeout 5 free > "$output_dir/$hostname-$timestamp"-mem.txt
+timeout 10 raspinfo > "$output_dir/$hostname-$timestamp"-raspinfo.txt
