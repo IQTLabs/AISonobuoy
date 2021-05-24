910bc366764238574b0afb63bf6edec31dc7280d
cglewis <clewis@iqt.org>
2021-05-24 09:16:54 -0700

    typo

--- PiBuoy/scripts/startup.sh
+++ PiBuoy/scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timeout 10 mount /dev/sda1 /flash
-timeout 60 /opt/BiggerBoat/PiBouy/scripts/gps.sh
+timeout 60 /opt/BiggerBoat/PiBuoy/scripts/gps.sh
 if [ $? -eq 1 ]
 then
   logger "Failed to bring up GPS, trying one more time"
@@ -17,5 +17,5 @@ timeout 10 stty -F /dev/ttyUSB1 speed 9600
 timeout 10 gpsd -n /dev/ttyUSB1 -F /var/run/gpsd.sock
 mkdir -p /flash/telemetry/pindrop
 amixer -D sysdefault cset name='ADC Capture Volume' 96,96
-pindrop --daemon --conf=/opt/BiggerBoat/PiBouy/config/pindrop.conf &
-python3 /opt/BiggerBoat/PiBouy/scripts/serial_ais.py &
+pindrop --daemon --conf=/opt/BiggerBoat/PiBuoy/config/pindrop.conf &
+python3 /opt/BiggerBoat/PiBuoy/scripts/serial_ais.py &
