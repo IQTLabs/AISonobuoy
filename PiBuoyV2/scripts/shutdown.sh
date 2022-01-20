8ffc219c3e70315a3932e36f0927b6b73809c681
cglewis <clewis@iqt.org>
2022-01-20 10:16:55 -0800

    add joystick option to shutdown

--- PiBuoyV2/scripts/shutdown.sh
+++ PiBuoyV2/scripts/shutdown.sh
@@ -2,7 +2,7 @@
 date >> /var/log/shutdown.log
 systemctl stop ais.service
 systemctl stop record.service
-/usr/bin/env bash /opt/AISonobuoy/PiBuoy/scripts/s3_prep.sh
+/usr/bin/env bash /opt/AISonobuoy/PiBuoyV2/scripts/s3_prep.sh
 sync
 sync
 sync
