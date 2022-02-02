947db6fa8578ff4d03eb72a8c1d265afe5b1bae2
Josh Bailey <josh@vandervecken.com>
2022-02-02 08:37:45 +0000

    record path.

--- PiBuoyV2/services/record/record.sh
+++ PiBuoyV2/services/record/record.sh
@@ -7,6 +7,6 @@ hostname=$HOSTNAME
 mkdir -p $flacdir
 timestamp=$(date +%s%3N)
 flacout=$hostname-$timestamp-hydrophone.flac
-arecord -q -D sysdefault -r 44100 -d 600 -f S16 -V mono - | ffmpeg -i -y -ac 1 -ar 16000 -sample_fmt s16 $flacdir/.$flacout
+arecord -q -D sysdefault -r 44100 -d 600 -f S16 -V mono - | ffmpeg -i - -y -ac 1 -ar 16000 -sample_fmt s16 $flacdir/.$flacout
 mv $flacdir/.$flacout $flacdir/$flacout
 sleep 10
