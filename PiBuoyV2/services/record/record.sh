1d7d386600613b05accb6f86a44b23be98b93fad
cglewis <clewis@iqt.org>
2022-02-06 10:27:58 -0800

    fix timestamps

--- PiBuoyV2/services/record/record.sh
+++ PiBuoyV2/services/record/record.sh
@@ -5,7 +5,7 @@ flacdir=/flash/telemetry/hydrophone
 /usr/bin/amixer sset ADC 40db
 hostname=$HOSTNAME
 mkdir -p $flacdir
-timestamp=$(date +%s%3N)
+timestamp=$(date +%s)
 flacout=$hostname-$timestamp-hydrophone.flac
 arecord -q -D sysdefault -r 44100 -d 600 -f S16 -V mono - | ffmpeg -i - -y -ac 1 -ar 16000 -sample_fmt s16 $flacdir/.$flacout
 tmpflacs=$(find $flacdir -type f -name .*.flac)
