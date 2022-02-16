9c48bcfc1386e27b4e055a91d5333054a30a9f78
Josh Bailey <josh@vandervecken.com>
2022-02-16 03:12:07 +0000

    add power coverage.

--- PiBuoyV2/services/power/power_app.py
+++ PiBuoyV2/services/power/power_app.py
@@ -3,14 +3,24 @@ import json
 import os
 import shutil
 import socket
+import stat
 import time
 
 
 class Power:
 
-    def __init__(self):
+    def __init__(self, data_dir='flash/telemetry/power', root_dir='/', uid=1000, gid=1000, time_sec=None):
         self.hostname = os.getenv("HOSTNAME", socket.gethostname())
-        self.data_dir = '/flash/telemetry/power'
+        self.root_dir = root_dir
+        self.data_dir = self.root_path(data_dir)
+        if time_sec is None:
+            time_sec = self.time_sec
+        self.time_sec = time_sec
+        self.uid = int(uid)
+        self.gid = int(gid)
+
+    def time_sec(self):
+        return time.time()
 
     def rename_dotfiles(self):
         for dotfile in glob.glob(os.path.join(self.data_dir, '.*')):
@@ -18,7 +28,8 @@ class Power:
             non_dotfile = os.path.join(self.data_dir, basename[1:])
             os.rename(dotfile, non_dotfile)
 
-    def write_data(self, timestamp, data):
+    def write_data(self, data):
+        timestamp = self.time_sec()
         tmp_filename = f'{self.data_dir}/.{self.hostname}-{timestamp}-power.json'
         with open(tmp_filename, 'a') as f:
             for key in data.keys():
@@ -47,8 +58,8 @@ class Power:
         return data["data"] / 1e3
 
 
-    def get_data(self, pj, data, time_sec=time.time):
-        timestamp = int(time_sec() * 1e3)
+    def get_data(self, pj, data):
+        timestamp = int(self.time_sec() * 1e3)
         try:
             status = pj.status.GetStatus()["data"]
             data["battery_charge"].append([pj.status.GetChargeLevel()["data"], timestamp])
@@ -75,22 +86,41 @@ class Power:
         return data
 
 
-    def main(self):
+    def get_pijuice(self):
         import pijuice  # pylint: disable=import-error # pytype: disable=import-error
+        return pijuice
+
+
+    def root_path(self, path):
+        return os.path.join(self.root_dir, path)
+
+
+    def poll_wait(self):
+        time.sleep(60)
+
+
+    def main(self, get_pijuice=None, poll_wait=None):
+        if get_pijuice is None:
+            get_pijuice = self.get_pijuice
+        if poll_wait is None:
+            poll_wait = self.poll_wait
+
+        pijuice = get_pijuice()
 
         os.makedirs(self.data_dir, exist_ok=True)
+        shutdown_path = self.root_path('home/pi/shutdown.sh')
 
         # copy files to host that are needed for pijuice
-        shutil.copyfile('/pijuice_config.JSON', '/var/lib/pijuice/pijuice_config.JSON')
-        shutil.copyfile('/shutdown.sh', '/home/pi/shutdown.sh')
+        shutil.copyfile(self.root_path('pijuice_config.JSON'), self.root_path('var/lib/pijuice/pijuice_config.JSON'))
+        shutil.copyfile(self.root_path('shutdown.sh'), shutdown_path)
 
         # fix permissions
-        os.system('chown 1000:1000 /home/pi/shutdown.sh')
-        os.system('chmod +x /home/pi/shutdown.sh')
+        os.chown(shutdown_path, self.uid, self.gid)
+        os.chmod(shutdown_path, os.stat(shutdown_path).st_mode | stat.S_IEXEC)
 
         data = self.init_data()
 
-        while not os.path.exists('/dev/i2c-1'):
+        while not os.path.exists(self.root_path('dev/i2c-1')):
             time.sleep(0.1)
 
         pj = pijuice.PiJuice(1, 0x14)
@@ -105,14 +135,18 @@ class Power:
 
         write_cycles = 1
         while True:
-            data = self.get_data(pj, data)
-            if write_cycles == 15:  # write out every 15 minutes
-                write_timestamp = int(time.time())
-                self.write_data(write_timestamp, data)
-                data = self.init_data()
-                write_cycles = 1
-            write_cycles += 1
-            time.sleep(60)  # 1 minute between checks
+            try:
+                data = self.get_data(pj, data)
+                if write_cycles == 15:  # write out every 15 minutes
+                    self.write_data(data)
+                    data = self.init_data()
+                    write_cycles = 1
+                write_cycles += 1
+                poll_wait()
+            except KeyboardInterrupt:
+                # flush unwritten data
+                self.write_data(data)
+                break
 
 
 if __name__ == '__main__':
