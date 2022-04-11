39c1eb81ca7967a79a55a9555a180f8182b38484
Raymond LeClair <raymond.leclair@springbok.io>
2022-04-11 08:41:05 -0400

    Ignore Jupyter lab checkpoint directories

--- DataScience/GpxAudioLabeler.py
+++ DataScience/GpxAudioLabeler.py
@@ -583,6 +583,55 @@ def main():
         default="collection-gpx.json",
         help="the path of the collection JSON file to load",
     )
+    """
+    The sample JSON document contains entries in the following format
+    which describe the method, and its parameters, used in creating
+    labeled samples from the collection. The method can be either
+    "clusters" or "conditionals".
+
+    If "clusters", the samples are all placed in the specified output
+    directory, and labeled with the start and stop times, and the
+    distance, heading, heading rate, and speed cluster averages.
+
+    If "conditionals", the samples are all placed in the output
+    directory and labeled with the start and stop times, and the
+    distance, heading, heading rate, and speed limits. Multiple
+    entries with the "conditionals" method will result in samples in a
+    set of labeled directories.
+
+    The maximum time delta between positions used to define a
+    contiguous audio sample is in seconds.
+
+    Format:
+    [
+        {
+            "name": "default",
+            "method": {
+                "type": "clusters",
+                "distance_n_clusters": 3,
+                "heading_n_clusters": 10,
+                "heading_dot_n_clusters": 2,
+                "speed_n_clusters": 4
+            },
+            "delta_t_max": 4.0,
+            "n_clips_max": 3,
+            "output_dir": "default"
+        },
+        {
+            "name": "close-north-stable-fast",
+            "method": {
+                "type": "conditionals",
+                "distance_limits": [0, 250],
+                "heading_limits": [-10, 0, 0, 10],
+                "heading_dot_limits": [0, 1],
+                "speed_limits": [10, 20]
+            },
+            "delta_t_max": 4.0,
+            "n_clips_max": 3,
+            "output_dir": "close-north-stable-fast"
+        }
+    ]
+    """
     parser.add_argument(
         "-s",
         "--sampling-filepath",
@@ -650,8 +699,7 @@ def main():
                     audio,
                     src_max_stop_t,
                     hyd_min_stop_t,
-                    Path(args.clip_home)
-                    / "no-boat"
+                    Path(args.clip_home) / "no-boat"
                     / f"{Path(hydrophone['name'].lower()).stem}-{src_max_stop_t}-{hyd_min_stop_t}-no-source.wav",
                 )
 
