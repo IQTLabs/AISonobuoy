73d1019638fe4acf249984c72088562e1221adf1
Raymond LeClair <rleclair@iqt.org>
2022-04-22 12:01:01 -0400

    Move download_object to S3Utilities

--- DataScience/AisWavLabeler.py
+++ DataScience/AisWavLabeler.py
@@ -14,70 +14,20 @@ import S3Utilities as s3
 
 
 # Logging configuration
-root = logging.getLogger()
-if not root.handlers:
+root_logger = logging.getLogger()
+if not root_logger.handlers:
     ch = logging.StreamHandler()
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
     ch.setFormatter(formatter)
-    root.addHandler(ch)
+    root_logger.addHandler(ch)
 
 logger = logging.getLogger("AisWavLabeler")
 logger.setLevel(logging.INFO)
 
 
-def download_object(download_path, bucket, s3_object):
-    """Download an object from an AWS S3 bucket to a local path.
-
-    Parameters
-    ----------
-    download_path : pathlib.Path()
-        The local path to which to download objects
-    bucket : str
-        The AWS S3 bucket
-    s3_object : dict
-        The AWS S3 object
-
-    Returns
-    -------
-    etag : str
-        The ETag of the AWS S3 object
-
-    Note that AWS S3 objects contain an ETag which is either an MD5
-    sum of the file, or an MD5 sum of the concatenated MD5 sums of
-    chunks of the file followed by a hypen and the number of chunks.
-
-    See:
-    https://zihao.me/post/calculating-etag-for-aws-s3-objects/
-    https://botocore.amazonaws.com/v1/documentation/api/latest/reference/response.html
-    """
-    if "-" in s3_object["ETag"]:
-        is_hyphenated = True
-        md5s = []
-        chunk_size = 8 * 1024 * 1024
-    else:
-        is_hyphenated = False
-        md5 = hashlib.md5()
-        chunk_size = 1024 * 1024
-    key = s3_object["Key"]
-    r = s3.get_object(bucket, key)
-    with open(download_path / key, "wb") as f:
-        for chunk in r["Body"].iter_chunks(chunk_size=chunk_size):
-            f.write(chunk)
-            if is_hyphenated:
-                md5s.append(hashlib.md5(chunk).digest())
-            else:
-                md5.update(chunk)
-    if is_hyphenated:
-        md5 = hashlib.md5(b"".join(md5s))
-        etag = f"{md5.hexdigest()}-{len(md5s)}"
-    else:
-        etag = md5.hexdigest()
-    return etag
-
-
-def download_objects(download_path, bucket, prefix=None, force=False, decompress=False):
+def download_buoy_objects(download_path, bucket, prefix=None, force=False, decompress=False):
     """Download all objects, optionally identified by their prefix, in
     an AWS S3 bucket to a local path. Check the ETag. Optionally
     decompress and, if appropriate, move extracted files by type.
@@ -206,7 +156,7 @@ if __name__ == "__main__":
     data_home = Path(args.data_home)
 
     # Download all files
-    download_objects(
+    download_buoy_objects(
         data_home,
         args.bucket,
         prefix=args.prefix,
