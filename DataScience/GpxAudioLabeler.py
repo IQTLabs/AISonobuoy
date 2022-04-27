6419d93f2bd53822cc05ce1a97c107320ac3ef37
Raymond LeClair <rleclair@iqt.org>
2022-04-27 10:15:23 -0400

    Remove collection and sampling documentation

--- DataScience/GpxAudioLabeler.py
+++ DataScience/GpxAudioLabeler.py
@@ -577,94 +577,16 @@ def main():
         default=str(Path("~").expanduser() / "Data" / "AISonobuoy"),
         help="the directory containing all GPX, WAV, and JSON files",
     )
-    """
-    The collection JSON contains information in the following format
-    to describe the sources and hydrophones used during the
-    collection.
-
-    Start and stop times are in seconds. Latitude, and longitude are
-    in degrees. Elevation is in meters.
-
-    Format:
-    {
-        "sources": [
-            {
-                "name": "suuntoapp-Motorsports-2022-02-22T18-09-01Z-track.gpx",
-                "start_t": 0,
-                "stop_t": 8889.0
-            }
-        ],
-        "hydrophones": [
-            {
-                "name": "Unit-01.WAV",
-                "lat": 0.0,
-                "lon": 0.0,
-                "ele": 0.0,
-                "start_t": 1200,
-                "stop_t": 9748
-            }
-        ]
-    }
-    """
     parser.add_argument(
         "-c",
         "--collection-filename",
-        default="collection.json",
+        default="collection-gpx.json",
         help="the path of the collection JSON file to load",
     )
-    """
-    The sample JSON document contains entries in the following format
-    which describe the method, and its parameters, used in creating
-    labeled samples from the collection. The method can be either
-    "clusters" or "conditionals".
-
-    If "clusters", the samples are all placed in the specified output
-    directory, and labeled with the start and stop times, and the
-    distance, heading, heading rate, and speed cluster averages.
-
-    If "conditionals", the samples are all placed in the output
-    directory and labeled with the start and stop times, and the
-    distance, heading, heading rate, and speed limits. Multiple
-    entries with the "conditionals" method will result in samples in a
-    set of labeled directories.
-
-    The maximum time delta between positions used to define a
-    contiguous audio sample is in seconds.
-
-    Format:
-    [
-        {
-            "name": "default",
-            "method": {
-                "type": "clusters",
-                "distance_n_clusters": 3,
-                "heading_n_clusters": 10,
-                "heading_dot_n_clusters": 2,
-                "speed_n_clusters": 4
-            },
-            "delta_t_max": 4.0,
-            "n_clips_max": 3,
-            "output_dir": "default"
-        },
-        {
-            "name": "close-north-stable-fast",
-            "method": {
-                "type": "conditionals",
-                "distance_limits": [0, 250],
-                "heading_limits": [-10, 0, 0, 10],
-                "heading_dot_limits": [0, 1],
-                "speed_limits": [10, 20]
-            },
-            "delta_t_max": 4.0,
-            "n_clips_max": 3,
-            "output_dir": "close-north-stable-fast"
-        }
-    ]
-    """
     parser.add_argument(
         "-s",
         "--sampling-filepath",
-        default=str(Path(__file__).parent / "data" / "sampling.json"),
+        default=str(Path(__file__).parent / "data" / "sampling-gpx.json"),
         help="the path of the sampling JSON file to process",
     )
     parser.add_argument(
@@ -706,7 +628,7 @@ def main():
     # Load file describing sampling cases
     sampling = lu.load_json_file(args.sampling_filepath)
 
-    # Consider each course
+    # Consider each source
     for source in collection["sources"]:
         if source["type"] != "file":
             raise Exception("Unexpected source type")
