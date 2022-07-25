389c612af3dd2867c79e149e9499e39b18c24804
Ari Chadda <achadda@iqt.org>
2022-07-25 13:12:07 -0400

    updated graphing code

--- DataScience/AisAudioLabeler.py
+++ DataScience/AisAudioLabeler.py
@@ -28,6 +28,10 @@ if not root_logger.handlers:
 logger = logging.getLogger("AisAudioLabeler")
 logger.setLevel(logging.INFO)
 
+# color-blind accessible color palette, https://gist.github.com/thriveth/8560036
+CB_color_cycle = ['#ff7f00', '#377eb8', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
 
 def download_buoy_objects(
     download_path, bucket, prefix=None, label=None, force=False, decompress=False
@@ -509,25 +513,34 @@ def plot_intervals(shp, hmd):
     None
 
     """
-    fig, axs = plt.subplots()
+    fig, axs = plt.subplots(figsize=(10,9), dpi=100)
 
     # Consider each ship
     n_ship = 0
+    label_set = set()
     for mmsi, statuses in shp.items():
         n_ship += 1
 
         # Consider each status for the current ship
         for status, intervals in statuses.items():
             if status in ["UnderWayUsingEngine"]:
-                color = "r"
+                color = CB_color_cycle[0] # orange 
+                label = "UnderWay Using Engine"
             elif status in ["AtAnchor", "Moored", "NotUnderCommand"]:
-                color = "b"
+                color = CB_color_cycle[1] # blue
+                label = "Anchored/Moored/Not Under Command"
             else:
-                color = "g"
+                color = CB_color_cycle[2] # green
+                label = "Other"
 
             # Plot each interval for the current ship and status
             for interval in intervals:
-                axs.plot(interval, [n_ship, n_ship], color)
+                if label not in label_set:
+                    label_set.add(label)    
+                else:
+                    label = ""
+                axs.plot(interval, [n_ship, n_ship], color, label=label)
+                
 
     # Consider each hydrophone metadta entry
     xlim = axs.get_xlim()
@@ -544,10 +557,13 @@ def plot_intervals(shp, hmd):
     # Label axes and show plot
     axs.set_xlabel("Timestamp [s]")
     axs.set_ylabel("Group")
+    plt.legend(bbox_to_anchor=(1,0), loc="lower right", 
+                bbox_transform=fig.transFigure, ncol=2)
+    plt.title('Recorded Ships with AIS Status')
     plt.show()
 
 
-def plot_histogram(ais, max_n_ships):
+def plot_histogram(ais, max_n_ships, bins=100):
     """Plot histogram of distance for times at which at most a
     specified maximum number of ships are reporting their status as
     underway.
@@ -564,13 +580,16 @@ def plot_histogram(ais, max_n_ships):
     None
 
     """
-    fig, axs = plt.subplots()
+    fig, axs = plt.subplots(figsize=(10,9), dpi=100)
+    underway_dists = ais.loc[(ais["shipcount_uw"] <= max_n_ships) & (ais["shipcount_uw"] > 0)]["distance"] 
     axs.hist(
-        ais.loc[ais["shipcount_uw"] <= max_n_ships, ["distance"]].to_numpy(), bins=100
+        underway_dists.to_numpy(), bins=bins
     )
-    axs.set_title("Distance")
+    axs.set_title("Count of Ships Reporting Underway by Distance")
     axs.set_xlabel("distance [m]")
     axs.set_ylabel("counts")
+    if len(underway_dists) > bins: 
+        plt.yscale("log")  
     plt.show()
 
 
