76e3113b234a18389f5d3ed81c254f8e53368c65
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 09:04:48 +0000

    Adding tarfile member sanitization to extractall()

--- DataScience/AisAudioLabeler.py
+++ DataScience/AisAudioLabeler.py
@@ -108,7 +108,26 @@ def download_buoy_objects(
                 logger.info(f"Skipping file {key}")
                 continue
             with tarfile.open(download_path / key) as f:
-                f.extractall(download_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, download_path)
                 names = f.getnames()
             logger.info(f"Decompressed file {key}")
 
